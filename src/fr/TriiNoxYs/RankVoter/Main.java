c1e081d4d867b7af308d67f777e2e923bc7857e8
TriiNoxYs <triinoxys@gmail.com>
2015-07-20 04:03:50 +0200

    Initial Commit

--- src/fr/TriiNoxYs/RankVoter/Main.java
+++ src/fr/TriiNoxYs/RankVoter/Main.java
@@ -6,7 +6,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
